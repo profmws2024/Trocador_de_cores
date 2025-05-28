52848b65f764e75c831f1181aa33ecb5933a153e
matheus <professormatheuswendel@gmail.com>
2025-05-28 09:37:25 -0300

    trocador

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("Vermelho");
+
+  botao.addEventListener("click", function () {
+    document.body.style.backgroundColor = "red";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("Verde");
+
+  botao.addEventListener("click", function () {
+    document.body.style.backgroundColor = "green";
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("Amarelo");
+
+  botao.addEventListener("click", function () {
+    document.body.style.backgroundColor = "yellow";
+  });
+});
+
